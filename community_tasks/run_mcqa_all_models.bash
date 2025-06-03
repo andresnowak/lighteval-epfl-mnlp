6f31fab4582b62ea3fb13937feacb58c8f4a26b6
Andres Eduardo Nowak de Anda <nowak@izar1.izar.cluster>
2025-06-03 11:30:12 +0200

    Added instruction finetuned mcqa and IF done only on completion loss

--- community_tasks/run_mcqa_all_models.bash
+++ community_tasks/run_mcqa_all_models.bash
@@ -56,3 +56,21 @@ lighteval accelerate \
     --output-dir ../output \
     ../model_configs/mcqa_text_model.yaml \
 "community|mnlp_mcqa_evals_mcqa_evals|0|0,community|mnlp_mcqa_evals_math_qa|0|0,community|mnlp_mcqa_evals_musr|0|0,community|mnlp_mcqa_evals_gpqa|0|0,community|mnlp_mcqa_evals_nlp4education|0|0,community|mnlp_mcqa_evals_arc_challenge|0|0,community|mnlp_mcqa_evals_arc_easy|0|0,community|mnlp_mcqa_evals_mmlu_pro|0|0,community|mnlp_mcqa_evals_mmlu|0|0"
+
+
+lighteval accelerate \
+    --eval-mode lighteval \
+    --save-details \
+    --custom-tasks "./mnlp_mcqa_evaluation.py" \
+    --output-dir ../output \
+    ../model_configs/IF_v2_model.yaml \
+"community|mnlp_mcqa_evals_mcqa_evals|0|0,community|mnlp_mcqa_evals_math_qa|0|0,community|mnlp_mcqa_evals_musr|0|0,community|mnlp_mcqa_evals_gpqa|0|0,community|mnlp_mcqa_evals_nlp4education|0|0,community|mnlp_mcqa_evals_arc_challenge|0|0,community|mnlp_mcqa_evals_arc_easy|0|0,community|mnlp_mcqa_evals_mmlu_pro|0|0,community|mnlp_mcqa_evals_mmlu|0|0"
+
+
+lighteval accelerate \
+    --eval-mode lighteval \
+    --save-details \
+    --custom-tasks "./mnlp_mcqa_evaluation.py" \
+    --output-dir ../output \
+    ../model_configs/IF_mcqa_model.yaml \
+"community|mnlp_mcqa_evals_mcqa_evals|0|0,community|mnlp_mcqa_evals_math_qa|0|0,community|mnlp_mcqa_evals_musr|0|0,community|mnlp_mcqa_evals_gpqa|0|0,community|mnlp_mcqa_evals_nlp4education|0|0,community|mnlp_mcqa_evals_arc_challenge|0|0,community|mnlp_mcqa_evals_arc_easy|0|0,community|mnlp_mcqa_evals_mmlu_pro|0|0,community|mnlp_mcqa_evals_mmlu|0|0"
